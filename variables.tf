8314dfffac58242b6b7e72a0085164fdeb6ebfa2
boussada19 <124534419+boussada19@users.noreply.github.com>
2025-07-10 18:06:12 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-
 variable "vsphere_user" {}
 variable "vsphere_password" {}
 variable "vsphere_server" {}
@@ -7,11 +6,8 @@ variable "datacenter" {}
 variable "datastore" {}
 variable "network" {}
 variable "template_name" {}
+variable "esxi_host" {}
 
 variable "vm_name" {}
 variable "vm_cpu" { type = number }
 variable "vm_memory" { type = number }
-variable "vm_disk" { type = number }
-variable "vm_ip" {}
-variable "vm_gateway" {}
-variable "admin_password" {}
