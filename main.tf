551695c85fb6c4c60b51d7588872eacfcc47c531
boussada19 <124534419+boussada19@users.noreply.github.com>
2025-07-10 21:44:04 +0200

    Update main.tf
    add cdrom to boot iso

--- main.tf
+++ main.tf
@@ -1,49 +1,43 @@
 provider "vsphere" {
-  user                 = "administrator@vsphere.local"
-  password             = "y#1>hwAsr%r,sx1"
-  vsphere_server       = "10.0.1.50"
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
   allow_unverified_ssl = true
 }
 
-# 1. Datacenter AUTO-INFRA
 data "vsphere_datacenter" "dc" {
   name = "AUTO-INFRA"
 }
 
-# 2. Datastore1
 data "vsphere_datastore" "datastore" {
   name          = "datastore1"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# 3. Hôte ESXi (10.1.1.10)
-data "vsphere_host" "esxi_host" {
-  name          = "10.1.1.10"
+data "vsphere_host" "host" {
+  name          = "10.0.1.10"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# 4. Réseau virtuel
 data "vsphere_network" "network" {
   name          = "ESXi-VM-Network"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# 5. Template Windows Server
 data "vsphere_virtual_machine" "template" {
   name          = "template-windows-server"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# 6. Création de la VM clonée
 resource "vsphere_virtual_machine" "vm" {
   name             = "vm-from-template"
-  resource_pool_id = data.vsphere_host.esxi_host.resource_pool_id
+  resource_pool_id = data.vsphere_host.host.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
 
-  num_cpus = 2
-  memory   = 4096
-  guest_id = data.vsphere_virtual_machine.template.guest_id
-  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+  num_cpus   = 2
+  memory     = 4096
+  guest_id   = data.vsphere_virtual_machine.template.guest_id
+  scsi_type  = data.vsphere_virtual_machine.template.scsi_type
 
   network_interface {
     network_id   = data.vsphere_network.network.id
@@ -51,12 +45,33 @@ resource "vsphere_virtual_machine" "vm" {
   }
 
   disk {
-    label            = "Disk0"
+    label            = "disk0"
     size             = data.vsphere_virtual_machine.template.disks[0].size
+    eagerly_scrub    = false
     thin_provisioned = data.vsphere_virtual_machine.template.disks[0].thin_provisioned
+  }
+
+  cdrom {
+    client_device = false
+    datastore_id  = data.vsphere_datastore.datastore.id
+    path          = "[datastore1] 17763.3650.221105-1748.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso"
   }
 
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      windows_options {
+        computer_name  = "vm-from-template"
+        admin_password = "Admin123!"
+      }
+
+      network_interface {
+        ipv4_address = "10.0.1.99"
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = "10.0.1.1"
+    }
   }
 }
