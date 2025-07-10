dac24253be075e3ab0e230f83437bcb0f4f849e2
boussada19 <124534419+boussada19@users.noreply.github.com>
2025-07-10 20:07:05 +0200

    Update main.tf
    changing the cluster information

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = ">= 2.5.0" # Important pour activer la personnalisation
+    }
+  }
+}
+
 provider "vsphere" {
   user                 = var.vsphere_user
   password             = var.vsphere_password
@@ -5,38 +14,45 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-data "vsphere_datacenter" "dc" {
-  name = var.datacenter
+# 📍 Récupère le datacenter "AUTO-INFRA"
+data "vsphere_datacenter" "datacenter" {
+  name = "AUTO-INFRA"
 }
 
+# 📦 Récupère le datastore "datastore1"
 data "vsphere_datastore" "datastore" {
-  name          = var.datastore
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "datastore1"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-data "vsphere_host" "esxi_host" {
-  name          = var.vsphere_host
-  datacenter_id = data.vsphere_datacenter.dc.id
+# 🧠 Récupère le cluster de calcul "automation-Infra"
+data "vsphere_compute_cluster" "cluster" {
+  name          = "Automation-Infra"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
+# 🌐 Récupère le réseau virtuel "VM Network"
 data "vsphere_network" "network" {
-  name          = var.network
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "ESXi-VM-Network"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
+# 📄 Récupère le template Windows existant "template-windows-server"
 data "vsphere_virtual_machine" "template" {
-  name          = var.template_name
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "template-windows-server"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
+# 🚀 Création de la VM à partir du template
 resource "vsphere_virtual_machine" "vm" {
-  name             = var.vm_name
-  resource_pool_id = data.vsphere_host.esxi_host.resource_pool_id
+  name             = "win-vm-001"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
 
-  num_cpus = var.vm_cpu
-  memory   = var.vm_memory
+  num_cpus = 2
+  memory   = 4096
   guest_id = data.vsphere_virtual_machine.template.guest_id
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
 
   network_interface {
     network_id   = data.vsphere_network.network.id
@@ -44,27 +60,27 @@ resource "vsphere_virtual_machine" "vm" {
   }
 
   disk {
-    label            = "disk0"
-    size             = var.vm_disk
-    eagerly_scrub    = false
-    thin_provisioned = true
+    label            = "Hard Disk 1"
+    size             = data.vsphere_virtual_machine.template.disks[0].size
+    thin_provisioned = data.vsphere_virtual_machine.template.disks[0].thin_provisioned
   }
 
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
-  }
 
-  customize {
-    windows_options {
-      computer_name  = var.vm_name
-      admin_password = var.admin_password
+    customize {
+      windows_options {
+        computer_name  = "win-vm-001"
+        admin_password = var.admin_password
+        time_zone      = 004  # Tunis/Paris (GMT+1)
+      }
+
+      network_interface {
+        ipv4_address = var.vm_ip
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = var.vm_gateway
     }
-
-    network_interface {
-      ipv4_address = var.vm_ip
-      ipv4_netmask = 24
-    }
-
-    ipv4_gateway = var.vm_gateway
   }
 }
