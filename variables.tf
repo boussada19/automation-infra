658ec51c3a67aeed2ee7c71e3f86417e4d3d6782
boussada19 <124534419+boussada19@users.noreply.github.com>
2025-07-10 20:07:43 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,16 +2,11 @@ variable "vsphere_user" {}
 variable "vsphere_password" {}
 variable "vsphere_server" {}
 
-variable "datacenter" {}
-variable "datastore" {}
-variable "network" {}
-variable "template_name" {}
-variable "vsphere_host" {}
+variable "admin_password" {}
 
-variable "vm_name" {}
-variable "vm_cpu" { type = number }
-variable "vm_memory" { type = number }
-variable "vm_disk" { type = number }
 variable "vm_ip" {}
 variable "vm_gateway" {}
-variable "admin_password" {}
+
+variable "vm_name" {
+  default = "win-vm-001"
+}
